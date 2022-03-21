e1e0ae1bf8a4551ce1e155647d21164e92e563f8
Mateo Victorica <3951@holbertonschool.com>
2022-03-21 15:58:50 -0300

    added ex8 main8

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * goes through a binary tree using post-order traversal
+ * @tree: pointer to the node to traverse
+ * @func: pointer to a function to call for each node
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+    if (!tree || !func || !(*func))
+        return;
+    if (tree->left)
+        binary_tree_postorder(tree->left, func);
+    if (tree->right)
+        binary_tree_postorder(tree->right, func);
+    (*func)(tree->n);
+}
