0c67799f3957e8e1a354d9819d9dd156bfd8a9e1
Mateo Victorica <3951@holbertonschool.com>
2022-03-21 17:43:10 -0300

    added ex 10 main10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * measures the depth of a binary tree
+ * @tree: pointer to the node to measure the depth
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+    size_t sum;
+
+    if (!tree)
+        return (0);
+    sum = binary_tree_depth(tree->parent);
+    return (sum + 1);
+}
