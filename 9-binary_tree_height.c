fd4533710e3a8845bf2dfbbab1b8255725cd54b1
Mateo Victorica <3951@holbertonschool.com>
2022-03-21 16:28:11 -0300

    added ex9 main9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ * measures the height of a binary tree
+ * @tree: pointer to the node to measure the height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    size_t leftsum;
+    size_t rightsum;
+
+    if (!tree)
+        return NULL;
+    if (!(tree->left) || !(tree->right))
+        return (0);
+    leftsum = binary_tree_height(tree->left);
+    rightsum = binary_tree_height(tree->right);
+    if (rightsum >= leftsum)
+        return (rightsum++);
+    else
+        return (leftsum++);
+}
